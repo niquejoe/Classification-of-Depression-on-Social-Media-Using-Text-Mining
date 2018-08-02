6d2c4f4b8e7859b3a8867b87be262ee4a09b8ab3
Nikie Jo <nikie.deocampo@gmail.com>
2018-08-02 11:06:02 +0900

    remove unused codes

--- depression_sentiment_analysis.py
+++ depression_sentiment_analysis.py
@@ -45,18 +45,6 @@ def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='Confusion matrix',
                           cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    #else:
-        #print('Confusion matrix, without normalization')
-
-    #print(cm)
-
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
